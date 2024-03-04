8f7eba4ef6c718efa486f92733453acee19e24cb
M-Bahy <Homelander.SolderBoy@gmail.com>
2024-03-04 23:33:06 +0200

    Documented main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+"""
+The `main` function is the entry point of the program. It reads the command line arguments and calls the `read_params` function to read the configuration file.
+It then creates an instance of the `VelodyneManager` class and calls the `run` method to start processing the pcap file.
+"""
+
 import argparse
 import yaml
 
-from lidar_manager import *
-
-GPS = True
+from lidar_manager import VelodyneManager
 
 
+# Reads the configuration file (params.yaml)
 def read_params(path):
     try:
         f = open(path, "rb")
@@ -16,21 +20,8 @@ def read_params(path):
     return params
 
 
-def main(args):
-    path = args["path"]
-    lidar_type = args["type"]
-    outdir = args["out_dir"]
-    config = args["config"]
-
-    params = read_params(config)
-
-    if "velodyne" in lidar_type.lower():
-        lidar_manager = VelodyneManager(lidar_type, path, outdir, params)
-
-    lidar_manager.run()
-
-
-if __name__ == "__main__":
+# Initializes the command line parser to read the inputted paths
+def init_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--path", help="Path to the pcap file", required=True)
     parser.add_argument(
@@ -39,9 +30,34 @@ if __name__ == "__main__":
     parser.add_argument(
         "-c", "--config", help="Path of the configuration file", required=True
     )
+    # Optional argument to specify the lidar type (default is VLP16)
     parser.add_argument(
-        "-t", "--type", help="Lidar name", required=False, default="velodyneVLP16"
+        "-t", "--type", help="Lidar name", required=False, default="VLP16"
     )
 
-    args = vars(parser.parse_args())
-    main(args)
+    return parser
+
+
+# Decode the command line arguments and create an instance of the VelodyneManager class to start processing the pcap file
+def main(args):
+    pcap_path = args["path"]
+    lidar_type = args["type"]
+    out_directory = args["out_dir"]
+    configuration_file = args["config"]
+
+    configuration_parameters = read_params(configuration_file)
+
+    if "VLP" in lidar_type.lower():
+        lidar_manager = VelodyneManager(
+            lidar_type, pcap_path, out_directory, configuration_parameters
+        )
+        lidar_manager.run()
+    else:
+        print("Lidar type not supported")
+
+
+# This method is responsible for starting the program by reading the command line arguments and sending them to the main function.
+if __name__ == "__main__":
+    parser = init_parser()
+    arguments = vars(parser.parse_args())
+    main(arguments)
